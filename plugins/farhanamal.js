82effb41826d4f72299e2ab8f781f62c2aa943d4
SPIRIT SER <96570722+SPIRITSER@users.noreply.github.com>
2022-04-29 11:11:20 +0530

    Update farhanamal.js

--- plugins/farhanamal.js
+++ plugins/farhanamal.js
@@ -33,7 +33,7 @@ Neotro.addCommand({pattern: 'help', fromMe: false, dontAddCommandList: true}, (a
        
        const button = {
         buttonText: '𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘',
-        description: "★𝐼𝑇𝜡_𝑀𝛯ꪶ𝑆𝑃𝐼𝑅𝐼𝑇_𝑆͢𝛯𝑅͎ꫂ★⁩",
+        description: "ꫝꫀꪶρ ꪑꫀ𝘴𝘴ꪖᧁꫀ💞",
         sections: sections,
         listType: 1
        }
